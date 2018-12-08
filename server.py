570e242a07d8697d857ffd08706bce5c6a22b815
Reah Rajmangal <reahrajmangal@gmail.com>
2018-12-08 16:41:23 -0500

    Add existing file

--- server.py
+++ server.py
@@ -1,39 +1,36 @@
-#!/usr/bin/env python
-
-# WS server example
-
 import asyncio
 import websockets
 import random
+import webbrowser
 
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 def createChatBot():
     chatbot = ChatBot('Barnard King', storage_adapter='chatterbot.storage.SQLStorageAdapter',
-     logic_adapters=[
-            "chatterbot.logic.MathematicalEvaluation",
-            "chatterbot.logic.TimeLogicAdapter",
-            {
-            "import_path": "chatterbot.logic.BestMatch",
-            "statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
-            "response_selection_method": "chatterbot.response_selection.get_first_response",
-            "threshold": 0.65,
-            "default_response": 'I am sorry, but I d not understand.'
-        }
-        ]
+                      logic_adapters=[
+                          {
+                              "import_path": "chatterbot.logic.BestMatch",
+                              "statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
+                              "response_selection_method": "chatterbot.response_selection.get_first_response",
+                              "threshold": 0.65,
+                              "default_response": 'I am sorry, but I do not understand.'
+                          },
+                          "chatterbot.logic.MathematicalEvaluation",
+                          "chatterbot.logic.TimeLogicAdapter"
+                      ],
     )
     chatbot.set_trainer(ChatterBotCorpusTrainer)
     chatbot.train("chatterbot.corpus.english")
     return chatbot
 
 async def hello(websocket, path):
+    global chatbot
     try:
-        chatbot = createChatBot()
         name = await websocket.recv()
         print(f"< {name}")
 
-        greeting = f"<b>Barnard:</b> Hello {name}! My name is Barnard. Ask me something!"
+        greeting = f"<b>Barnard:</b> Hello {name}! My name is Barnard. Let's chat!"
         await websocket.send(greeting)
 
         while True:
@@ -48,7 +45,8 @@ async def hello(websocket, path):
         exit(1)
 
 
-
 start_server = websockets.serve(hello, 'localhost', 5678)
+chatbot= createChatBot()
+webbrowser.open("chatbot.html")
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
