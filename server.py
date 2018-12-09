de9413d198a82a2ad42697ec611eb88d9183476e
Reah Rajmangal <reahrajmangal@gmail.com>
2018-12-08 20:57:44 -0500

    finish clean up

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import webbrowser
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+
+# creates a chatbot object trained on math, time, and english conversations...
 def createChatBot():
     chatbot = ChatBot('Barnard King', storage_adapter='chatterbot.storage.SQLStorageAdapter',
                       logic_adapters=[
@@ -19,15 +21,17 @@ def createChatBot():
                           "chatterbot.logic.MathematicalEvaluation",
                           "chatterbot.logic.TimeLogicAdapter"
                       ],
-    )
+                      )
     chatbot.set_trainer(ChatterBotCorpusTrainer)
     chatbot.train("chatterbot.corpus.english")
     return chatbot
 
 
+# coroutine handler the server runs until forced to quit
 async def chat(websocket, path):
-    global chatbot
+    global chatbot  # make global since param cannot accept chatbot and load chatbot happens outside this function
     try:
+        # first grab name from client and set conversation stamps
         name = await websocket.recv()
         print(f"< {name}")
 
@@ -35,6 +39,7 @@ async def chat(websocket, path):
         await websocket.send(greeting)
 
         while True:
+            # begin receiving messages to process a response for and send
             message = await websocket.recv()
             format_message = "<b>{}:</b> {}".format(name, message)
             await websocket.send(format_message)
@@ -42,12 +47,15 @@ async def chat(websocket, path):
             bot_message = "<b>Barnard:</b> {}".format(message)
             await websocket.send(bot_message)
     except:
+        # catch if a connection was closed either by closing page/quitting, exit server after
         print("connection was closed")
         exit(1)
 
 
 start_server = websockets.serve(chat, 'localhost', 5678)
-chatbot= createChatBot()
+chatbot = createChatBot()
+print ("finished training!")
+# open chatbot after the bot has been trained
 webbrowser.open("chatbot.html")
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
